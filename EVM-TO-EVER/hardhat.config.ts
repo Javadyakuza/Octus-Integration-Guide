5e4bb83fec5f737f0f607f2469c86b1ec4f78bcc
yakuza <javadyakuzaa@gmail.com>
2023-06-25 01:39:09 +0330

    SCRIPTS_UPDATED

--- EVM-TO-EVER/hardhat.config.ts
+++ EVM-TO-EVER/hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
-import { ethers } from "hardhat";
 require("dotenv").config();
 
 const config: HardhatUserConfig = {
